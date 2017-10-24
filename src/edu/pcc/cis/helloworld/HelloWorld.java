33073d75595de91f69b2fa615b103f4d7777f485
Dylan Young <dyroyo@outlook.com>
2017-10-24 13:59:59 -0700

    Added printDate method, which is called in main.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,13 +1,21 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         printGreeting();
+        printDate();
     }
 
     private static void printGreeting()
     {
        String greeting = "Hello World! This is my first repository!";
        System.out.println(greeting);
+    }
+
+    private static void printDate()
+    {
+        System.out.println(new Date());
     }
 }
