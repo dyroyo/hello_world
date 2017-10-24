faad63a9159e1dcc5cb7d2a7c892f383383eb95f
Dylan Young <dyroyo@outlook.com>
2017-10-24 13:54:01 -0700

    Added main method which calls the new printGreeting method

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,13 @@
 package edu.pcc.cis.helloworld;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        printGreeting();
+    }
+
+    private static void printGreeting()
+    {
+       String greeting = "Hello World! This is my first repository!";
+       System.out.println(greeting);
+    }
 }
